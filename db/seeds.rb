84aeed259c2f5a8a04b6ab8308b32a124046064b
Alexa <alexa.anderson@outlook.com>
2017-03-27 01:13:13 -0500

    seeds and clean layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@ BOARDS_PER_USER = 3
 LISTS_PER_BOARD = 3
 CARDS_PER_LIST = 3
 
+BOARDS = ['A', 'B', 'C'].map { |letter| "Project #{letter}"}
+LISTS = ['A', 'B', 'C'].map { |letter| "Goal #{letter}"}
+CARDS = ['A', 'B', 'C'].map { |letter| "Task #{letter}"}
+
 puts 'creating users'
 NUM_USERS.times do |num|
   user = User.create(
@@ -24,17 +28,17 @@ puts 'creating boards'
 User.all.each do |user|
   BOARDS_PER_USER.times do |num|
     user.boards.create(
-      title: Faker::Hipster.word.capitalize,
-      description: Faker::Hacker.say_something_smart
+      title: BOARDS[num%3],
+      description: "Exciting description for #{BOARDS[num%3]}"
     )
   end
 end
 
 puts 'creating lists'
 Board.all.each do |board|
-  LISTS_PER_BOARD.times do
+  LISTS_PER_BOARD.times do |num|
     board.lists.create(
-      title: Faker::GameOfThrones.house
+      title: LISTS[num%3]
     )
   end
   member = User.all.sample
@@ -43,10 +47,10 @@ end
 
 puts 'creating cards'
 List.all.each do |list|
-  CARDS_PER_LIST.times do
+  CARDS_PER_LIST.times do |num|
     card = list.cards.create(
-      name: Faker::GameOfThrones.character,
-      description: Faker::StarWars.quote,
+      name: CARDS[num%3],
+      description: "Exciting description for #{CARDS[num%3]}",
       priority: Faker::Number.between(1, 5)
     )
     card.members = list.board.users
